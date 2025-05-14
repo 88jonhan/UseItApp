37007798465675b9cc82a728cb7f86073f96f32e
88jonhan <88jonhan@skola.boras.se>
2025-05-14 14:46:28 +0200

    Material config

--- UseItApp.API/ClientApp/src/app/app.component.ts
+++ UseItApp.API/ClientApp/src/app/app.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { CommonModule } from '@angular/common';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ClientApp';
+export class AppComponent implements OnInit {
+  title = 'lending-app-client';
+  isLoggedIn = false;
+  username = '';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.isLoggedIn$.subscribe(isLoggedIn => {
+      this.isLoggedIn = isLoggedIn;
+    });
+
+    this.authService.currentUser$.subscribe(user => {
+      this.username = user?.username || '';
+    });
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
